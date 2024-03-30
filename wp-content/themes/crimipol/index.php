8d28c656c28ea7454176b7c746100bbd5f8c69b2
Yago <104085125+YagoGomez83@users.noreply.github.com>
2024-03-29 23:48:13 -0300

    Trabajando en la pagina principal

--- wp-content/themes/crimipol/index.php
+++ wp-content/themes/crimipol/index.php
@@ -1,36 +1,14 @@
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+<?php get_header(); ?>
+<main class="container">
+  <?php
+  while (have_posts()) : the_post();
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
+    the_title();
 
-<body>
-  <header class="header">
-    <nav class="nav">
-      <div class="links">
-        <a href="./servicios.html" class="link">Servicios</a>
-        <a href="./Blog.html" class="link">Blog</a>
-      </div>
-      <h1 class="header_nav">Crimipol</h1>
-      <div class="links">
-        <a href="./servicios.html" class="link">Nosotros</a>
-        <a href="./Blog.html" class="link">Contactos</a>
-      </div>
-    </nav>
-  </header>
-  <main>
-    <?php
-    while (have_posts()) : the_post();
-
-      the_title();
-
-      the_content();
-    endwhile;
-    ?>
-  </main>
+    the_content();
+  endwhile;
+  ?>
+</main>
 </body>
 
 </html>
